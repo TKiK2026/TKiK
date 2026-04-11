219c0289a1e4e7c88ad4f533ae2f191071128672
Jakub Bialek <jbialek@student.agh.edu.pl>
2026-04-12 01:55:21 +0200

    W scratch2 podstawowa obsługa języka

---
+++ projekt/scratch2.py
@@ -0,0 +1,283 @@
+import ply.lex as lex
+import ply.yacc as yacc
+tokens = (
+    "ID",
+    "STRING",
+    "PLUS",
+    "MINUS",
+    "ASSIGN_OP",
+    "MAKE",
+    "PRINT",
+    "NUMBER",
+    "TIMES",
+    "OVER",
+    "AMERICA",
+    "GREAT",
+    "FACT",
+    "LIE",
+    "LBRACE",
+    "RBRACE",
+    "IF",
+    "ELSE",
+    "GREATER",
+    "LESS",
+    "EQUAL",
+    "QUESTION",
+    "GE",
+    "LE",
+    "GT",
+    "LT",
+    "EQ"
+)
+t_LBRACE = r':'
+t_RBRACE = r'!'
+t_ignore = ' \t'
+t_QUESTION = r'\?'
+t_GE = r'>='
+t_LE = r'<='
+t_EQ = r'=='
+t_ASSIGN_OP = r'='
+t_GT = r'>'
+t_LT = r'<'
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    val = t.value.lower()
+    keywords = {
+        'is': 'ASSIGN_OP', 'are': 'ASSIGN_OP', '=': 'ASSIGN_OP',
+        'make': 'MAKE',
+        'say': 'PRINT', 'tell': 'PRINT',
+        'plus': 'PLUS', '+': 'PLUS',
+        'minus': 'MINUS', '-': 'MINUS',
+        'times': 'TIMES', '*': 'TIMES',
+        'over': 'OVER', '/': 'OVER',
+        'america':'AMERICA',
+        'great':'GREAT',
+        'fact':'FACT',
+        'lie':'LIE',
+        'if': 'IF',
+        'else': 'ELSE',
+        'greater': 'GREATER',
+        'less': 'LESS',
+        'equal': 'EQUAL',
+    }
+    t.type = keywords.get(val, 'ID')
+    return t
+
+def t_STRING(t):
+    r'\"[^\"]*\"'
+    t.value = t.value[1:-1]
+    return t
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    print(f"Illegal character '{t.value[0]}'")
+    t.lexer.skip(1)
+
+precedence = (
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'TIMES', 'OVER'),
+)
+def p_program(p):
+    '''program : body AMERICA ASSIGN_OP GREAT'''
+    p[0] = p[1]
+def p_body(p):
+    '''body : statement
+            | body statement'''
+    if len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = p[1] + [p[2]]
+def p_statement(p):
+    '''statement : assignment
+                | print_statement
+                | if_statement'''
+    p[0] = p[1]
+def p_if_statement(p):
+    '''if_statement : IF comparison LBRACE body RBRACE
+                    | IF comparison LBRACE body RBRACE ELSE LBRACE body RBRACE'''
+    if len(p) == 6:
+        p[0] = ('if', p[2], p[4])
+    else:
+        p[0] = ('if_else', p[2], p[4], p[8])
+def p_comparison(p):
+    '''comparison : expression ASSIGN_OP expression
+                  | expression GREATER expression
+                  | expression LESS expression
+                  | expression GREATER EQUAL expression
+                  | expression LESS EQUAL expression
+                  | expression EQ expression
+                  | expression GE expression
+                  | expression LE expression
+                  | expression GT expression
+                  | expression LT expression'''
+    if len(p) == 4:
+        # Mapowanie symboli i słów na nazwy zrozumiałe dla evaluate
+        type_map = {
+            'ASSIGN_OP': 'ASSIGN_OP', 'EQ': 'ASSIGN_OP',
+            'GE': 'GREATER_EQUAL', 'LE': 'LESS_EQUAL',
+            'GT': 'GREATER', 'LT': 'LESS'
+        }
+        op_type = p.slice[2].type
+        p[0] = (type_map.get(op_type, op_type), p[1], p[3])
+    else:
+        # Obsługa 'greater equal' / 'less equal'
+        op = 'GREATER_EQUAL' if p[2].lower() == 'greater' else 'LESS_EQUAL'
+        p[0] = (op, p[1], p[4])
+def p_assignment(p):
+    '''assignment : ID ASSIGN_OP expression
+                    | MAKE ID expression'''
+    if len(p) == 4:
+        if p[1].lower()=="make":
+            p[0] = ("assign",p[2],p[3])
+        else:
+            p[0] = ("assign",p[1],p[3])
+def p_print(p):
+    '''print_statement : PRINT expression'''
+    p[0] = ('print',p[2])
+def p_expression_binop(p):
+    '''expression : expression PLUS expression
+                    | expression MINUS expression
+                    | expression TIMES expression
+                    | expression OVER expression'''
+    p[0] = (p.slice[2].type,p[1],p[3])
+def p_expression_value(p):
+    '''expression : value'''
+    p[0] = p[1]
+
+
+def p_value(p):
+    '''value : NUMBER
+             | STRING
+             | ID
+             | LIE
+             | FACT
+             | expression ASSIGN_OP expression QUESTION
+             | expression GREATER expression QUESTION
+             | expression LESS expression QUESTION
+             | expression GREATER EQUAL expression QUESTION
+             | expression LESS EQUAL expression QUESTION
+             | expression EQ expression QUESTION
+             | expression GE expression QUESTION
+             | expression LE expression QUESTION
+             | expression GT expression QUESTION
+             | expression LT expression QUESTION'''
+
+    if len(p) == 2:
+        if p.slice[1].type == 'FACT':
+            p[0] = True
+        elif p.slice[1].type == 'LIE':
+            p[0] = False
+        else:
+            p[0] = p[1]
+
+    elif len(p) == 5:
+        type_map = {
+            'ASSIGN_OP': 'ASSIGN_OP',
+            'EQ': 'ASSIGN_OP',
+            'GE': 'GREATER_EQUAL',
+            'LE': 'LESS_EQUAL',
+            'GT': 'GREATER',
+            'LT': 'LESS'
+        }
+        token_type = p.slice[2].type
+        p[0] = (type_map.get(token_type, token_type), p[1], p[3])
+    else:
+        op = 'GREATER_EQUAL' if p[2].lower() == 'greater' else 'LESS_EQUAL'
+        p[0] = (op, p[1], p[4])
+
+def p_error(p):
+    print("Syntax Error")
+lexer = lex.lex()
+
+
+def evaluate(exp,variables):
+    if not isinstance(exp, tuple):
+        if isinstance(exp, int) or isinstance(exp, bool):
+            return exp
+        if isinstance(exp, str) and exp in variables:
+            return variables[exp]
+        return exp
+    op,l,r = exp
+    op = op.upper()
+    left_val = evaluate(l,variables)
+    right_val = evaluate(r,variables)
+
+    if op == 'PLUS':  return left_val + right_val
+    if op == 'MINUS': return left_val - right_val
+    if op == 'TIMES': return left_val * right_val
+    if op == 'OVER':
+        if right_val == 0:
+            print("Error: Dividing by zero? Not in this country!")
+            return 0
+        return left_val / right_val
+    if op == 'ASSIGN_OP': return left_val == right_val
+    if op == 'GREATER': return left_val > right_val
+    if op == 'LESS': return left_val < right_val
+    if op == 'GREATER_EQUAL': return left_val >= right_val
+    if op == 'LESS_EQUAL': return left_val <= right_val
+    return 0
+
+def run_interpreter(instructions,variables = None):
+    if variables is None:
+        variables = {}
+
+    for instr in instructions:
+        type = instr[0]
+
+        if type == "assign":
+            # instr to ('assign', nazwa, wartość)
+            var_name = instr[1]
+            value = evaluate(instr[2],variables)
+            variables[var_name] = value
+
+        elif type == "print":
+            target = instr[1]
+            val = evaluate(target,variables)
+            if val is True:
+                print("Fact")
+            elif val is False:
+                print("False")
+            else:
+                print(val)
+        elif type == "if":
+            if evaluate(instr[1],variables):
+                run_interpreter(instr[2],variables)
+        elif type == "if_else":
+            if evaluate(instr[1],variables):
+                run_interpreter(instr[2],variables)
+            else:
+                run_interpreter(instr[3],variables)
+    return variables
+parser = yacc.yacc()
+
+
+data = """
+wall = 100
+wall is 150
+make is_huge wall >= 50 ?
+say "Is the wall huge using symbols?"
+say is_huge
+wall are 100
+if wall = 100  :
+    say "The wall is exactly 100 (checked with words)"
+!
+if wall == 100 :
+    say "Checking with == symbol also works"
+!
+make wall 150
+say wall is 150?
+America is great
+"""
+
+
+result = parser.parse(data, lexer=lexer)
+print(result)
+if result:
+    print("--- Program Wykonany ---")
+    run_interpreter(result)
