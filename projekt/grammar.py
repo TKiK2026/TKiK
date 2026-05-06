bf76bce7db400f1e70668c306ed2833e01443029
Jakub Bialek <jbialek@student.agh.edu.pl>
2026-05-06 13:33:55 +0200

    grammar.py

--- projekt/grammar.py
+++ projekt/grammar.py
@@ -1,10 +1,12 @@
 def p_program(p):
     """program : body AMERICA_GREAT
                | AMERICA_GREAT"""
+    p[0] = [] if len(p) == 2 else p[1]
 
 def p_body(p):
     """body : statement
             | body statement"""
+    p[0] = [p[1]] if len(p) == 2 else p[1] + [p[2]]
 
 def p_statement(p):
     """statement : assignment
@@ -12,22 +14,35 @@ def p_statement(p):
                  | if_statement
                  | loop_statement
                  | BREAK"""
+    p[0] = ("break",) if p.slice[1].type == "BREAK" else p[1]
 
 def p_assignment(p):
     """assignment : ID ASSIGN_OP_SIGN expression
                   | ID ASSIGN_OP_WORD expression
                   | MAKE ID expression"""
+    if p.slice[1].type == "MAKE":
+        p[0] = ("assign", p[2], p[3])
+    else:
+        p[0] = ("assign", p[1], p[3])
 
 def p_print_statement(p):
     """print_statement : PRINT expression"""
+    p[0] = ("print", p[2])
 
 def p_if_statement(p):
     """if_statement : IF expression LBRACE body RBRACE
                     | IF expression LBRACE body RBRACE ELSE LBRACE body RBRACE
                     | IF expression LBRACE body RBRACE ELSE if_statement"""
+    if len(p) == 6:
+        p[0] = ("if", p[2], p[4])
+    elif len(p) == 10:
+        p[0] = ("if_else", p[2], p[4], p[8])
+    else:
+        p[0] = ("if_else_if", p[2], p[4], [p[7]])
 
 def p_loop_statement(p):
     """loop_statement : AS_LONG_AS expression LBRACE body RBRACE"""
+    p[0] = ("while", p[2], p[4])
 
 def p_expression_binop(p):
     """expression : expression PLUS expression
@@ -36,6 +51,7 @@ def p_expression_binop(p):
                   | expression OVER expression
                   | expression AND expression
                   | expression OR expression"""
+    p[0] = (p.slice[2].type, p[1], p[3])
 
 def p_expression_compare(p):
     """expression : expression EQ expression
@@ -44,15 +60,19 @@ def p_expression_compare(p):
                   | expression GT expression
                   | expression LT expression
                   | expression ASSIGN_OP_WORD expression"""
+    p[0] = ("COMPARE", p.slice[2].type, p[1], p[3])
 
 def p_expression_unary(p):
     """expression : NOT expression"""
+    p[0] = ("NOT", p[2])
 
 def p_expression_question(p):
     """expression : expression QUESTION"""
+    p[0] = ("QUESTION", p[1])
 
 def p_expression_group(p):
     """expression : LPAREN expression RPAREN"""
+    p[0] = p[2]
 
 def p_expression_value(p):
     """expression : NUMBER
@@ -60,4 +80,10 @@ def p_expression_value(p):
                   | ID
                   | FACT
                   | LIE"""
-    
+    p[0] = p[1]
+
+def p_error(p):
+    if p:
+        print(f"Syntax Error near '{p.value}' (line {p.lineno})")
+    else:
+        print("Syntax Error at EOF")
